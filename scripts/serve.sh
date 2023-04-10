f7c105c223bf5d92ba2d5fdc21e56f2a78da9ef3
Dan Cascaval <dan@cascaval.us>
2023-04-10 13:27:32 -0700

    update serve script

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -9,11 +9,6 @@ check-install () {
 }
 
 check-install "live-server"
-check-install "json-caching-proxy"
-
-# Launch the caching proxy for sourcemaps
-# dos2unix ~/.config/yarn/global/node_modules/json-caching-proxy/bin.js
-json-caching-proxy -u https://raw.githubusercontent.com -p 8080 -l -a &
 
 # Launch the main server
 live-server --port=4000 --watch="index.html","main.css","dist/","js/target/scala-3.2.1/scalajs-bundler/main/monet-fastopt.js" --no-browser
